3d4840c374a9be45bed707e671e8ce1275761ace
Robin Shi <robin.shi@eightdevelopment.com>
2019-10-28 07:50:25 +1300

    show result properly

--- merger.py
+++ merger.py
@@ -5,12 +5,19 @@ import os.path
 import subprocess
 import time
 
+# global variables
 from_dir = ""
 to_dir = ""
 has_failure = False
-total_file_size = 0
+
+# total
+total_size_of_files = 0
 total_num_of_files = 0
 total_num_of_failure = 0
+
+# copied
+total_num_of_copied_files = 0
+total_size_of_copied_files = 0
 
 '''
 Check if target file already exist.
@@ -28,6 +35,9 @@ def file_is_exist(src, dst):
 			return False
 	return False
 
+'''
+check if command line parameters are valid
+'''
 def check_parameters():
 	global from_dir
 	global to_dir
@@ -72,24 +82,46 @@ def get_relative_path(base_dir, target_dir):
 	rel_path = os.path.relpath(target_dir, common_prefix)
 	return rel_path
 
+string_on_screen = ""
 def copy_file(src, target):
-	global has_failure
 	global total_num_of_files
-	global total_file_size
+	global total_num_of_failure
+	global total_size_of_files
+	global total_num_of_copied_files
+	global total_size_of_copied_files
+	global string_on_screen
+	
 	try:
+		# clear previous string from screen
+		back_str = '\b'*len(string_on_screen)
+		# print string_on_screen
+		sys.stdout.write(back_str)
+		
+
+		total_num_of_files += 1
+		file_size = os.path.getsize(src)
+		total_size_of_files += file_size
+		string_on_screen = "Verifying [files: {} ] [size: {}]\t{}\r".format(total_num_of_files, sizeof_fmt(total_size_of_files), src)
+		sys.stdout.write(string_on_screen)
+		sys.stdout.flush()
+		
 		if file_is_exist(src, target):
 			return
-		subprocess.check_call(["cp", src, target])
-		total_num_of_files += 1
-		total_file_size += os.path.getsize(src)
-		sys.stdout.write("Copying [files: {} ] [size: {}]\r".format(total_num_of_files, sizeof_fmt(total_file_size)))
-		sys.stdout.flush()
-	except Exception as e:
-		print 'Copy Failure!', src, '->', target
-		has_failure = True
-		total_num_of_failure += 1
 
-def create_folder_is_not_exist(path):
+		subprocess.check_call(["cp", src, target])
+		total_num_of_copied_files += 1
+		total_size_of_copied_files += file_size
+		string_on_screen = "Copying [files: {} ] [size: {}]\t{}\r".format(total_num_of_copied_files, sizeof_fmt(total_size_of_copied_files), src)
+		sys.stdout.write(string_on_screen)
+		sys.stdout.flush()
+
+	except Exception as e:
+		print 'Error!', src, '->', target
+		print e.message
+		total_num_of_failure += 1
+		
+
+def create_folder_if_not_exist(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 
@@ -107,30 +139,22 @@ def iterate_files():
 			continue
 		
 		relative_target_folder = get_relative_path(from_dir, dirName)
-		# print('Found relative_target_folder: %s' % relative_target_folder)
-		# print('Found absolute directory: %s' % dirName)
-		# TODO create target directory if needed
-		create_folder_is_not_exist(os.path.join(to_dir, relative_target_folder))
+		create_folder_if_not_exist(os.path.join(to_dir, relative_target_folder))
 
 		for fname in fileList:
-			# print('\t%s' % get_relative_path(from_dir, os.path.join(dirName, fname)))
 			absolute_src_file = os.path.join(dirName, fname)
 			relative_src_file = get_relative_path(from_dir, absolute_src_file)
 
-			# print('\t%s' % relative_src_file)
 			absolute_target_file = os.path.join(to_dir, relative_src_file)
 			copy_file(absolute_src_file, absolute_target_file)
 
-########################################
-########################################
-########################################
 start_time = time.time()
 check_parameters()
 iterate_files()
-if has_failure:
-	print "DONE! Failure exists!"
-else:
-	print "\nDONE! Succes! ==> {} files copied! [{}]".format(total_num_of_files, sizeof_fmt(total_file_size))
-print time.strftime("Time elapsed: %H:%M:%S", time.gmtime(time.time() - start_time))
 
-
+print "\n\n=========== MISSION COMPLETE ==========="
+print "Total Files: \t\t{}\t[{}]".format(total_num_of_files,sizeof_fmt(total_size_of_files))
+print "Files Copied: \t\t{}\t[{}]".format(total_num_of_copied_files, sizeof_fmt(total_size_of_copied_files))
+print "Num of Failures:\t{}".format(total_num_of_failure)
+print time.strftime("Time elapsed: \t\t%H:%M:%S", time.gmtime(time.time() - start_time))
+print
